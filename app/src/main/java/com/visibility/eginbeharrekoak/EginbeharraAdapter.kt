d9a85b5cfbe834592ab4687d783bfb91d92edc45
aitorzubiri <aitorgaillard99@gmail.com>
2025-10-09 13:10:05 +0200

    eginbeharren zerrenda ondo kudeatu eta adapterrari onclicklistener pasa

--- app/src/main/java/com/visibility/eginbeharrekoak/EginbeharraAdapter.kt
+++ app/src/main/java/com/visibility/eginbeharrekoak/EginbeharraAdapter.kt
@@ -5,21 +5,41 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.visibility.eginbeharrekoak.databinding.ItemEginbeharraBinding
 
-class EginbeharraAdapter(private val eginbeharrak: List<Eginbeharra>) :
-    RecyclerView.Adapter<EginbeharraAdapter.EginbeharraViewHolder>() {
+// Class honek balio du recyclerview barruan dauden itemak informazioz populatzeko
+// json fitxeroan egungo dira fitxero honetako eginbehar guztiak
+// Fitxero honek List<Eginbeharra> lista jasoko du nun listan daden json fitxategiko datuak
 
-    class EginbeharraViewHolder(val binding: ItemEginbeharraBinding) : RecyclerView.ViewHolder(binding.root)
 
+//Gehituko dugu lambda funtzio bat konstruktorean: onEginbeharraClick
+class EginbeharraAdapter(
+    private val eginbeharrak: List<Eginbeharra>,
+    private val onEginbeharraClick: (Eginbeharra) -> Unit
+) : RecyclerView.Adapter<EginbeharraAdapter.EginbeharraViewHolder>() {
+
+    class EginbeharraViewHolder(val binding: ItemEginbeharraBinding) :
+        RecyclerView.ViewHolder(binding.root)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EginbeharraViewHolder {
-        val binding = ItemEginbeharraBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemEginbeharraBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return EginbeharraViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: EginbeharraViewHolder, position: Int) {
-        val eginbeharra = eginbeharrak.get(position)
+        val eginbeharra = eginbeharrak[position]
+
         holder.binding.textViewIzenburua.text = eginbeharra.izenburua
         holder.binding.textViewDeskripzioa.text = eginbeharra.deskripzioa
         holder.binding.checkboxDone.isChecked = eginbeharra.egina
+
+        // 2. Ezarri OnClickListener-a CheckBox-ari
+        holder.binding.checkboxDone.setOnClickListener {
+            // Klik egitean, egoera aldatu
+            eginbeharra.egina = holder.binding.checkboxDone.isChecked
+            onEginbeharraClick(eginbeharra)
+        }
     }
 
     override fun getItemCount() = eginbeharrak.size
--- app/src/main/java/com/visibility/eginbeharrekoak/EginbeharrekoakActivity.kt
+++ app/src/main/java/com/visibility/eginbeharrekoak/EginbeharrekoakActivity.kt
@@ -1,47 +1,104 @@
 package com.visibility.eginbeharrekoak
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import java.io.IOException
 import com.visibility.eginbeharrekoak.databinding.ActivityEginbeharrekoakBinding
+import java.io.IOException
 
 class EginbeharrekoakActivity : AppCompatActivity() {
 
+    // Binding klasea, layout elementuekin komunikatzeko
+    private lateinit var binding: ActivityEginbeharrekoakBinding
+
+    // RecyclerView-rako adapterra
+    private lateinit var adapter: EginbeharraAdapter
+
+    // Eginbeharren zerrenda, lista aldakorra (mutable) da, ondo gorde eta kudeatzeko
+    private lateinit var eginbeharrenLista: MutableList<Eginbeharra>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_eginbeharrekoak)
-        val binding = ActivityEginbeharrekoakBinding.inflate(layoutInflater)
+
+        // Layoutaren binding-a sortu eta ezarri
+        binding = ActivityEginbeharrekoakBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // JSON-etik eginbeharrak kargatu
-        val eginbeharrenLista = kargatuEginbeharrakJson()
+        // Eginbeharren zerrenda kargatu, lehenik bilatzen du dispositiboaren fitxategi lokalean
+        // eta ez badago, assets-en dagoena kargatzen du
+        eginbeharrenLista = kargatuEginbeharrakLocalJson()
 
-        // Egiaztatu datuak ondo kargatu diren Logcat-en
-        if (eginbeharrenLista != null) {
-            for (eginbeharra in eginbeharrenLista) {
-                Log.d("Eginbeharrak", "Izenburua: ${eginbeharra.izenburua}, Eginda: ${eginbeharra.egina}")
+        // Adapterra sortu eta callback-a definitu:
+        // Eginbeharra markatzen denean (checkbox-a aktibatzen denean),
+        // eginbeharrak ezabatuko dira zerrendatik eta eguneratuko da fitxategi lokaleko JSON-a
+        adapter = EginbeharraAdapter(eginbeharrenLista) { eginbeharra ->
+            // Eginbeharraren indizea aurkitu zerrendan
+            val index = eginbeharrenLista.indexOf(eginbeharra)
+            // Baldintza: eginbeharra egina bada eta zerrendan badago
+            if (eginbeharra.egina && index != -1) {
+                // Eginbeharra ezabatu zerrendatik
+                eginbeharrenLista.removeAt(index)
+                // Adapterrari ezabapena jakinarazi, RecyclerView eguneratzeko
+                adapter.notifyItemRemoved(index)
+                // Zerrenda eguneratua gorde fitxategi lokalean JSON formatuan
+                guardarEginbeharrakJson(eginbeharrenLista)
             }
-        } else {
-            Log.e("Eginbeharrak", "Errorea JSON fitxategia kargatzean.")
         }
-        // sortu recyclerview aldagaia
-        val adapter: EginbeharraAdapter = EginbeharraAdapter(eginbeharrenLista)
-        binding.recyclerViewProjects.setAdapter(adapter)
+
+        // RecyclerView konfiguratu: Layout manager-a eta adapterra ezarri
+        binding.recyclerViewProjects.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewProjects.adapter = adapter
     }
 
-    private fun kargatuEginbeharrakJson(): List<Eginbeharra> {
+    /**
+     * Assets karpetatik JSON fitxategia kargatu eta Eginbeharra motako MutableList bihurtu
+     */
+    private fun kargatuEginbeharrakJson(): MutableList<Eginbeharra> {
         val jsonString: String
         try {
+            // "egitekoak.json" fitxategia irakurri assets karpetatik
             jsonString = assets.open("egitekoak.json").bufferedReader().use { it.readText() }
         } catch (ioException: IOException) {
             ioException.printStackTrace()
-            return emptyList()
+            // Arazo bat badago, zerrenda hutsa itzuli
+            return mutableListOf()
         }
 
-        val listType = object : TypeToken<List<Eginbeharra>>() {}.type
+        // JSON-a MutableList<Eginbeharra> bihurtzeko motaren deskribapena
+        val listType = object : TypeToken<MutableList<Eginbeharra>>() {}.type
+        // Gson erabiliz JSON deserializatu eta zerrenda itzuli
         return Gson().fromJson(jsonString, listType)
+    }
+
+    /**
+     * Eginbeharrak JSON formatuan serializeatu eta fitxategi lokal batean gorde
+     * Horrela aldaketak persistitzen dira
+     */
+    private fun guardarEginbeharrakJson(lista: List<Eginbeharra>) {
+        // Zerrenda JSON string bihurtu
+        val jsonString = Gson().toJson(lista)
+        // Fitxategi lokala irekita idazteko eta JSON-a bertan idatzi
+        openFileOutput("egitekoak.json", MODE_PRIVATE).use {
+            it.write(jsonString.toByteArray())
+        }
+    }
+
+    /**
+     * Lehenik fitxategi lokaletik JSON-a kargatu saiatu,
+     * fitxategia ez badago, assets-etik kargatu jatorrizko zerrenda
+     */
+    private fun kargatuEginbeharrakLocalJson(): MutableList<Eginbeharra> {
+        return try {
+            // Fitxategi lokala irakurri
+            val jsonString = openFileInput("egitekoak.json").bufferedReader().use { it.readText() }
+            val listType = object : TypeToken<MutableList<Eginbeharra>>() {}.type
+            // JSON deserializatu eta itzuli
+            Gson().fromJson(jsonString, listType)
+        } catch (e: IOException) {
+            // Fitxategia ez badago, asset-etik kargatu
+            kargatuEginbeharrakJson()
+        }
     }
 }
